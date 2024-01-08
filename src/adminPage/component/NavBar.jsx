7e9524432a604edf07f4f7de8cb2ccfa8c0ebaf8
nguyenvo811 <nguyenvo709@gmail.com>
2024-01-08 16:44:57 +0700

    nothing

--- src/adminPage/component/NavBar.jsx
+++ src/adminPage/component/NavBar.jsx
@@ -49,8 +49,8 @@ export default function NavBar() {
 									<path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
 								</svg>
 							</button>
-							<a href="/" className="flex ml-2 md:mr-24">
-								<div className="cursor-pointer w-[40px] h-[40px] relative" onClick={() => navigate("/")}>
+							<a onClick={() => navigate("/")} className="flex ml-2 md:mr-24">
+								<div className="cursor-pointer w-[40px] h-[40px] relative" >
 								<img className="w-full h-full object-center object-cover" src={Logo} />
 							</div>
 								<span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">NHÀ YẾN THÂN THI</span>
